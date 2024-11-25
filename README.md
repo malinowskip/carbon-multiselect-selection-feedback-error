b27551402be8b8b09fa249d86b2c27da7df5b18d
Paweł Malinowski <p@malinowski.pm>
2024-11-25 13:31:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+git clone git@github.com:malinowskip/carbon-multiselect-selection-feedback-error.git &&
+cd carbon-multiselect-selection-feedback-error &&
+pnpm install &&
+pnpm dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import 'carbon-components-svelte/css/g90.css';
+	import { MultiSelect } from 'carbon-components-svelte';
+</script>
+
+<main>
+	<p>
+		If <code>selectionFeedback</code> is set to <code>top-after-reopen</code> (default), selected
+		items should be moved to the top after making the selection, closing and then re-opening the
+		dropdown (<a
+			target="_blank"
+			href="https://react.carbondesignsystem.com/?path=/story/components-multiselect--default"
+			>React reference</a
+		>).
+	</p>
+	<hr />
+	<MultiSelect
+		titleText="Contact"
+		label="Select contact methods..."
+		items={[
+			{ id: '0', text: 'Slack' },
+			{ id: '1', text: 'Email' },
+			{ id: '2', text: 'Fax' }
+		]}
+		selectionFeedback="top-after-reopen"
+	/>
+</main>
+
+<style>
+	:global(body) {
+		display: flex;
+		justify-content: center;
+	}
+	main {
+		padding: 2rem;
+		align-self: center;
+		width: 100%;
+		max-width: 900px;
+	}
+</style>
